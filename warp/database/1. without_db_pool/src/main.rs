73fc96895a95e5caa5b0eb480f6df78908157e01
steadylearner <32325099+steadylearner@users.noreply.github.com>
2020-01-22 10:23:28 -0500

    Could refactor the first example of the database with macro. Should find how to remove crate etc prefix.

--- warp/database/1. without_db_pool/src/main.rs
+++ warp/database/1. without_db_pool/src/main.rs
@@ -29,11 +29,61 @@ mod db_connection;
 // For example, $cargo test list_post -- --nocapture
 #[cfg(test)] mod tests;
 
-// Make get_post, create_post, delete_post, update_post
-// While you refer to the links given above.
-
 // Use some test driven development with files at tests/ and CURL commands.
 // If you still can't make it work, don't modulze the Warp API and test them here first.
+
+// Make data first with post.sql
+// Use macros to ignore type problems and make them sharable to tests/
+#[macro_export]
+macro_rules! list_posts {
+    () => {
+        // $curl 0.0.0.0:8000/api/post/v1
+        post_route::list()
+            .and_then(post_handler::list)
+    }
+}
+
+#[macro_export]
+macro_rules! get_post {
+    () => {
+        // $curl 0.0.0.0:8000/api/post/v1/1
+        post_route::get()
+            .and_then(post_handler::get)
+    }
+}
+
+#[macro_export]
+macro_rules! create_post {
+    () => {
+        // curl -X POST localhost:8000/api/post/v1 -H "Content-Type: application/json" -d '{ "title": "When can I work with programming?", "body": "Should find someone recognize my skills." }'
+        // and $curl 0.0.0.0:8000/api/post/v1
+        // To see it created.
+        post_route::create()
+            .and_then(post_handler::create)
+    }
+}
+
+#[macro_export]
+macro_rules! update_post {
+    () => {
+        // curl -X PUT 0.0.0.0:8000/api/post/v1/1 -H "Content-Type: application/json" -d '{ "title": "test update", "body": "test update" }'
+        // and $curl 0.0.0.0:8000/api/post/v1/1
+        // To see it updated.
+        post_route::update()
+            .and_then(post_handler::update)
+    }
+}
+
+#[macro_export]
+macro_rules! delete_post {
+    () => {
+        // $curl -X DELETE 0.0.0.0:8000/api/post/v1/1 and $curl 0.0.0.0:8000/api/post/v1
+        // To see it deleted.
+        post_route::delete()
+            .and_then(post_handler::delete)
+    }
+}
+
 #[tokio::main]
 async fn main() {
     let target: String = "0.0.0.0:8000".parse().unwrap();
@@ -45,38 +95,11 @@ async fn main() {
     // let repeat = post_route::repeat()
     //     .and_then(post_handler::repeat);
 
-    // Make data first with post.sql
-
-    // $curl 0.0.0.0:8000/api/post/v1
-    let list_posts = post_route::list()
-        .and_then(post_handler::list);
-
-    // $curl 0.0.0.0:8000/api/post/v1/1
-    let get_post = post_route::get()
-        .and_then(post_handler::get);
-
-    // curl -X POST localhost:8000/api/post/v1 -H "Content-Type: application/json" -d '{ "title": "When can I work with programming?", "body": "Should find someone recognize my skills." }'
-    // and $curl 0.0.0.0:8000/api/post/v1
-    // To see it created.
-    let create_post = post_route::create()
-        .and_then(post_handler::create);
-
-    // curl -X PUT 0.0.0.0:8000/api/post/v1/1 -H "Content-Type: application/json" -d '{ "title": "test update", "body": "test update" }'
-    // and $curl 0.0.0.0:8000/api/post/v1/1
-    // To see it updated.
-    let update_post = post_route::update()
-        .and_then(post_handler::update);
-
-    // $curl -X DELETE 0.0.0.0:8000/api/post/v1/1 and $curl 0.0.0.0:8000/api/post/v1
-    // To see it deleted.
-    let delete_post = post_route::delete()
-        .and_then(post_handler::delete);
-
-    let post_api = list_posts
-        .or(get_post)
-        .or(create_post)
-        .or(update_post)
-        .or(delete_post);
+    let post_api = list_posts!()
+        .or(get_post!())
+        .or(create_post!())
+        .or(update_post!())
+        .or(delete_post!());
 
     let end = post_api.with(warp::log("post_api"));
 
